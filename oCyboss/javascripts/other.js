68c010b703a10f9a1adc715aaecadfde41f59c38
minhvo0o <vdminh@penphy.edu.vn>
2022-04-17 22:29:08 +0700

    Reconstruct Code / Clean Code

---
+++ oCyboss/javascripts/other.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+};
